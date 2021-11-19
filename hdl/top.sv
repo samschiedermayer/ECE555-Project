28c456f62782090a9d55646659190d65721c53ec
John Wirth <jjwirth2@wisc.edu>
2021-11-18 20:40:11 -0600

    modified comments

--- hdl/top.sv
+++ hdl/top.sv
@@ -5,9 +5,7 @@ module top(
 	output [1:0] out
 );
 
-// Instantiation of an MLP module
-// mlp mlp(x0, x1, w00, w01, w10, w11, w20, w21, u00, u10, u20, out);
-
+// Implementation of an MLP module
 wire [3:0] y0, y1;
 
 // Neuron 0
@@ -17,6 +15,6 @@ neuron n0(.x0(x0), .x1(x1), .w0(w00), .w1(w10), .w2(w20), .out(y0));
 neuron n1(.x0(x0), .x1(x1), .w0(w01), .w1(w11), .w2(w21), .out(y1));
 
 // Neuron Out
-neuron mlp_out(.x0(y0), .x1(y1), .w0(u00), .w1(u10), .w2(u20), .out(out)); // Neuron 1
+neuron mlp_out(.x0(y0), .x1(y1), .w0(u00), .w1(u10), .w2(u20), .out(out));
 
 endmodule
