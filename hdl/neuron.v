056ba6e8a2aa4589d363f7ed36bae2eacd79a839
MichaelSexton21 <michaeljsexton21@gmail.com>
2021-11-18 19:48:02 -0600

    fixed error

--- hdl/neuron.v
+++ hdl/neuron.v
@@ -14,8 +14,8 @@ mult imult_1(.x(x0), .w(w0), .out(mult1_out));
 mult imult_2(.x(x1), .w(w1), .out(mult2_out));
 
 // Adders
-adder iadder(.A(mult1_out), .B(mult2_out), .C(1'b0), .S(adder1_out));
-adder iadder(.A(adder1_out), .B(w2), .C(1'b0), .S(adder2_out));
+adder iadder_1(.A(mult1_out), .B(mult2_out), .C(1'b0), .S(adder1_out));
+adder iadder_2(.A(adder1_out), .B(w2), .C(1'b0), .S(adder2_out));
 
 // Multiplexer
 not (not_d_3, adder2_out[3]);
